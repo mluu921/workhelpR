4294501fb47caf3b0c4cc4c4892f1ea60f263eb6
Luu <mluu921@gmail.com>
2018-04-04 11:53:39 -0700

    updated make surv plot with additional arguments

--- R/surv_helper.R
+++ R/surv_helper.R
@@ -13,7 +13,8 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-make_surv_plot <- function(fit, data, title, legend_label, break_x_by = 12, pval_coord = c(0, 0.05)) {
+make_surv_plot <- function(data, fit, title, legend_label, break_x_by = 12, pval_coord = c(0, 0.05), legend_coord = c(.9, .9), plot_table_ratio = c(.8, .2),
+                           pval_text = T, xlim = NULL, risk_table_text_size = 5) {
   p <- survminer::ggsurvplot(
     fit = fit,
     data = data,
@@ -22,21 +23,21 @@ make_surv_plot <- function(fit, data, title, legend_label, break_x_by = 12, pval
     break.time.by = break_x_by,
     tables.y.text = F,
     censor = F,
-    pval = T,
+    pval = pval_text,
     pval.coord = pval_coord,
     pval.size = 8,
     surv.scale = "percent",
-    legend = c(.8, .1),
+    legend = legend_coord,
     palette = "Set1",
-    tables.height = 0.10,
     xlab = "Time (Months)",
+    xlim = xlim,
     legend.labs = legend_label,
-    fontsize = 7,
+    fontsize = risk_table_text_size,
     font.x = c(20,'bold'),
     font.y = c(20,'bold'),
     legend.title = '',
     font.tickslab = 18,
-    ggtheme = theme_survminer() +
+    ggtheme = survminer::theme_survminer() +
       theme(
         legend.title = element_blank(),
         legend.justification = c("center"),
@@ -55,6 +56,6 @@ make_surv_plot <- function(fit, data, title, legend_label, break_x_by = 12, pval
 
   p$plot <- p$plot + guides(colour = guide_legend(override.aes = list(size=5, alpha = 1)))
 
-  p <- cowplot::plot_grid(p$plot, p$table, nrow = 2, align = 'v', rel_heights = c(.9, .1))
+  p <- cowplot::plot_grid(p$plot, p$table, nrow = 2, align = 'v', rel_heights = plot_table_ratio)
   return(p)
 }
