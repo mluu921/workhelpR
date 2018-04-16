39132ba3158dd95b3b26551cd2ed1c906fc72179
Luu <mluu921@gmail.com>
2018-04-16 10:36:02 -0700

    add function to clean string names to file

--- R/utility_helper.R
+++ R/utility_helper.R
@@ -8,34 +8,4 @@ make_proj_folder <- function() {
 }
 
 
-clean_names_to_file <- function (string,
-                                 case = c(
-                                   "snake",
-                                   "lower_camel",
-                                   "upper_camel",
-                                   "screaming_snake",
-                                   "lower_upper",
-                                   "upper_lower",
-                                   "all_caps",
-                                   "small_camel",
-                                   "big_camel",
-                                   "old_janitor",
-                                   "parsed",
-                                   "mixed"
-                                 ))
-{
-  case <- match.arg(case)
 
-  old_names <- string
-  new_names <- old_names %>% gsub("'", "", .) %>% gsub("\"",
-                                                       "", .) %>% gsub("%", ".percent_", .) %>% gsub("#", ".number_",
-                                                                                                     .) %>% gsub("^[[:space:][:punct:]]+", "", .) %>% make.names(.) %>%
-    snakecase::to_any_case(
-      case = case,
-      sep_in = "\\.",
-      transliterations = c("Latin-ASCII"),
-      parsing_option = 4
-    )
-
-  return(new_names)
-}
